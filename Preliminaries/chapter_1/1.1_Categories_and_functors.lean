80907af178dcde0074a18fa71f0107002381950e
XipanXiao <XipanXiao@users.noreply.github.com>
2025-05-06 20:37:24 -0700

    Update 1.1_Categories_and_functors.lean

--- Preliminaries/chapter_1/1.1_Categories_and_functors.lean
+++ Preliminaries/chapter_1/1.1_Categories_and_functors.lean
@@ -45,3 +45,80 @@ noncomputable instance : Group (Aut' A) where
     apply Subtype.ext
     rw [mul_def, one_def]
     simp
+
+def toFun {A B : C} (φ : A ≅ B) (f: CategoryTheory.Aut A) : CategoryTheory.Aut B := ⟨
+  φ.inv ≫ f.hom ≫ φ.hom,
+  φ.inv ≫ f.inv ≫ φ.hom,
+  by simp,
+  by simp
+⟩
+
+def invFun {A B : C} (φ : A ≅ B) (g: CategoryTheory.Aut B) : CategoryTheory.Aut A := ⟨
+  φ.hom ≫ g.hom ≫ φ.inv,
+  φ.hom ≫ g.inv ≫ φ.inv,
+  by simp,
+  by simp
+⟩
+
+lemma to_inv_compose {A B : C} {φ : A ≅ B} {g: CategoryTheory.Aut B } : toFun φ (invFun φ g) = g := by
+    calc
+      toFun φ (invFun φ g) = ⟨
+        φ.inv ≫ (φ.hom ≫ g.hom ≫ φ.inv) ≫ φ.hom,
+        φ.inv ≫ (φ.hom ≫ g.inv ≫ φ.inv) ≫ φ.hom,
+        by simp,
+        by simp
+      ⟩ := by rfl
+      _ = ⟨
+        g.hom,
+        g.inv,
+        by simp,
+        by simp
+      ⟩ := by simp
+      _ = g := rfl
+
+lemma inv_to_compose {A B : C} {φ : A ≅ B} {f: CategoryTheory.Aut A } : invFun φ (toFun φ f) = f := by
+    calc
+      invFun φ (toFun φ f) = ⟨
+        φ.hom ≫ (φ.inv ≫ f.hom ≫ φ.hom) ≫ φ.inv,
+        φ.hom ≫ (φ.inv ≫ f.inv ≫ φ.hom) ≫ φ.inv,
+        by simp,
+        by simp
+            ⟩ := by rfl
+      _ = ⟨
+        f.hom,
+        f.inv,
+        by simp,
+        by simp
+      ⟩ := by simp
+      _ = f := rfl
+
+lemma map_mul' {A B : C} {φ : A ≅ B} (f g: CategoryTheory.Aut A) : toFun φ (f * g) = toFun φ f * toFun φ g := by
+    calc
+      toFun φ (f * g) = ⟨
+        φ.inv ≫ ((f * g).hom) ≫ φ.hom,
+        φ.inv ≫ ((f * g).inv) ≫ φ.hom,
+        by simp,
+        by simp
+      ⟩ := by rfl
+      _ = ⟨
+        φ.inv ≫ (g.hom ≫ f.hom) ≫ φ.hom,
+        φ.inv ≫ (f.inv ≫ g.inv) ≫ φ.hom,
+        by simp,
+        by simp
+      ⟩ := by rfl
+      _ = ⟨
+        (φ.inv ≫ g.hom ≫ φ.hom) ≫ (φ.inv ≫ f.hom ≫ φ.hom),
+        (φ.inv ≫ f.inv ≫ φ.hom) ≫ (φ.inv ≫ g.inv ≫ φ.hom),
+        by simp,
+        by simp
+      ⟩ := by simp
+      _ = toFun φ f * toFun φ g := by rfl
+
+/--  Show that two isomorphic objects have isomorphic automorphism groups.  -/
+example {A B : C} (φ : A ≅ B) : CategoryTheory.Aut A ≃* CategoryTheory.Aut B :=
+  { toFun := toFun φ,
+    invFun := invFun φ,
+    left_inv := fun f => by exact inv_to_compose
+    right_inv := fun f => by exact to_inv_compose
+    map_mul' := map_mul'
+    }
